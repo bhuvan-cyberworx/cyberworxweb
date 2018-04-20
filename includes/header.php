94b9dfac2ace82cfcb9e56284ba95646cf029f96
bhuvan-cyberworx <bhuvan@cyberworx.in>
2018-04-20 17:45:01 +0530

    Changes

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Including Bootstrap CSS CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Including Materialize CSS  -->
+    <link href="assets/css/materialize.css" rel="stylesheet">
 </head>
 <body>
     
