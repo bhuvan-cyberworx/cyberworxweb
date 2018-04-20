70a98dae0f31ea42a705aec66b0cbbb1495dd538
bhuvan-cyberworx <bhuvan@cyberworx.in>
2018-04-20 18:41:15 +0530

    Added website resources

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
+<!-- Including JQuery -->
 
 <!-- Including bootstrap bundle CDN -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js" integrity="sha384-lZmvU/TzxoIQIOD9yQDEpvxp6wEU32Fy0ckUgOH4EIlMOCdR823rg4+3gWRwnX1M" crossorigin="anonymous"></script>
 <!-- Including Materialize JS file -->
-<script type="text/javascript" src="assets/js/materialize.js"
+<script type="text/javascript" src="assets/js/materialize.js">
 </body>
 </html>
